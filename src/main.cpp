6147f289424637f0a609ade70989ad897df6ae0a
kkuez <doerping@gmx.de>
2021-08-01 08:59:15 +0200

    Init.

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,18 @@
 #include <IRremoteESP8266.h>
 #include <IRrecv.h>
 #include <IRutils.h>
+#include <EEPROM.h>
 
-int outOneUSeconds = 1000;
-int outTwoUSeconds = 1000;
-int outThreeUSeconds = 1000;
-bool on = true;
+int outOneUSeconds = 750;
+int outTwoUSeconds = 750;
+int outThreeUSeconds = 750;
+bool onToggle = true;
+//array with high pin number
+bool on[D8];
+bool waitToPersist = false;
+// [key][values for pin D5, D6, D7]
+int persistetValues[4][3];
+uint64_t lastPressed;
 
 const int irRecPin = D3;
 const uint64_t KEY_POWER = 3974070404;
@@ -27,6 +34,7 @@ const uint64_t KEY_VOL_DOWN = 2537318624;
 const uint64_t KEY_CHAN_UP = 2625382880;
 const uint64_t KEY_CHAN_DOWN = 457390884;
 const uint64_t KEY_ENTER = 1008447872;
+const long KEY_ENCORE = 18446744073709551615;
 
 IRrecv irRecv(irRecPin);
 decode_results irResults;
@@ -34,18 +42,31 @@ decode_results irResults;
 void processIRCommand(uint64_t cmdInt);
 void performLEDPWM();
 void toggleOnOff();
-void increasePWM(int *var, int val);
-void decreasePWM(int *var, int val);
+void increasePWM(int *var, int val, int pin);
+void decreasePWM(int *var, int val, int pin);
+void persist(uint64_t pin);
+void load(uint64_t pin);
+
+struct State
+{
+  int red;
+  int green;
+  int blue;
+};
 
 void setup()
 {
   Serial.begin(115200);
   irRecv.enableIRIn();
 
+  //D5= RED!!
   pinMode(D5, OUTPUT);
   pinMode(D6, OUTPUT);
   pinMode(D7, OUTPUT);
 
+  on[D5] = true;
+  on[D6] = true;
+  on[D7] = true;
   Serial.println("Starting...");
 }
 
@@ -53,7 +74,15 @@ void loop()
 {
   if (irRecv.decode(&irResults))
   {
-    processIRCommand(irResults.value);
+    if (irResults.value == KEY_ENCORE && lastPressed != 0)
+    {
+      processIRCommand(lastPressed);
+    }
+    else
+    {
+      processIRCommand(irResults.value);
+    }
+
     irRecv.resume();
   }
   delay(1);
@@ -63,6 +92,12 @@ void loop()
 
 void processIRCommand(uint64_t cmdInt)
 {
+  //If not waiting to persist, remove the flag
+  if (waitToPersist && !(cmdInt == KEY_SEVEN || cmdInt == KEY_EIGHT || cmdInt == KEY_NINE || cmdInt == KEY_ZERO || cmdInt == KEY_ENCORE))
+  {
+    waitToPersist = false;
+  }
+
   switch (cmdInt)
   {
   case KEY_POWER:
@@ -70,52 +105,148 @@ void processIRCommand(uint64_t cmdInt)
     break;
 
   case KEY_DOWN:
-    decreasePWM(&outOneUSeconds, 50);
-    decreasePWM(&outTwoUSeconds, 50);
-    decreasePWM(&outThreeUSeconds, 50);
+    decreasePWM(&outOneUSeconds, 50, D5);
+    decreasePWM(&outTwoUSeconds, 50, D6);
+    decreasePWM(&outThreeUSeconds, 50, D7);
+    lastPressed = cmdInt;
     break;
 
   case KEY_UP:
-    increasePWM(&outOneUSeconds, 50);
-    increasePWM(&outTwoUSeconds, 50);
-    increasePWM(&outThreeUSeconds, 50);
+    increasePWM(&outOneUSeconds, 50, D5);
+    increasePWM(&outTwoUSeconds, 50, D6);
+    increasePWM(&outThreeUSeconds, 50, D7);
+    lastPressed = cmdInt;
     break;
 
   case KEY_ONE:
     Serial.printf("KEY_ONE: %d \n", outOneUSeconds);
-    increasePWM(&outOneUSeconds, 50);
+    increasePWM(&outOneUSeconds, 50, D5);
+    lastPressed = cmdInt;
     break;
 
   case KEY_TWO:
     Serial.printf("KEY_TWO: %d \n", outTwoUSeconds);
-    increasePWM(&outTwoUSeconds, 50);
+    increasePWM(&outTwoUSeconds, 50, D6);
+    lastPressed = cmdInt;
     break;
 
   case KEY_THREE:
     Serial.printf("KEY_THREE: %d \n", outThreeUSeconds);
-    increasePWM(&outThreeUSeconds, 50);
+    increasePWM(&outThreeUSeconds, 50, D7);
+    lastPressed = cmdInt;
     break;
   case KEY_FOUR:
     Serial.printf("KEY_FOUR: %d \n", outOneUSeconds);
-    decreasePWM(&outOneUSeconds, 50);
+    decreasePWM(&outOneUSeconds, 50, D5);
+    lastPressed = cmdInt;
     break;
 
   case KEY_FIVE:
     Serial.printf("KEY_FIVE: %d \n", outTwoUSeconds);
-    decreasePWM(&outTwoUSeconds, 50);
+    decreasePWM(&outTwoUSeconds, 50, D6);
+    lastPressed = cmdInt;
     break;
   case KEY_SIX:
     Serial.printf("KEY_SIX: %d \n", outThreeUSeconds);
-    decreasePWM(&outThreeUSeconds, 50);
+    decreasePWM(&outThreeUSeconds, 50, D7);
+    lastPressed = cmdInt;
+    break;
+  case KEY_SEVEN:
+  case KEY_EIGHT:
+  case KEY_NINE:
+  case KEY_ZERO:
+    if (waitToPersist)
+    {
+      persist(cmdInt);
+      waitToPersist = false;
+    }
+    else
+    {
+      load(cmdInt);
+    }
+    lastPressed = 0;
+    break;
+  case KEY_ENTER:
+    waitToPersist = true;
+    lastPressed = 0;
     break;
   default:
+    Serial.println(cmdInt);
+    lastPressed = 0;
     break;
   }
 }
 
+void persist(uint64_t key)
+{
+  int keyStore;
+
+  switch (key)
+  {
+  case KEY_SEVEN:
+    keyStore = 7;
+    break;
+  case KEY_EIGHT:
+    keyStore = 8;
+    break;
+  case KEY_NINE:
+    keyStore = 9;
+    break;
+  case KEY_ZERO:
+    keyStore = 0;
+    break;
+
+  default:
+    return;
+    break;
+  }
+
+  State state = {outOneUSeconds, outTwoUSeconds, outThreeUSeconds};
+  Serial.printf("Writing to EEPROM: key %d, red %d, green %d, blue %d\n", keyStore, outOneUSeconds, outTwoUSeconds, outThreeUSeconds);
+  EEPROM.begin(512);
+  EEPROM.put(keyStore, state);
+  EEPROM.commit();
+  EEPROM.end();
+}
+
+void load(uint64_t key)
+{
+int keyStore;
+
+  switch (key)
+  {
+  case KEY_SEVEN:
+    keyStore = 7;
+    break;
+  case KEY_EIGHT:
+    keyStore = 8;
+    break;
+  case KEY_NINE:
+    keyStore = 9;
+    break;
+  case KEY_ZERO:
+    keyStore = 0;
+    break;
+
+  default:
+    return;
+    break;
+  }
+
+  State state;
+  EEPROM.begin(512);
+  EEPROM.get(keyStore, state);
+  EEPROM.end();
+  Serial.printf("Read from EEPROM: key %d, red %d, green %d, blue %d\n", keyStore, state.red, state.green, state.blue);
+  /* if(state == NULL) {
+  } else {
+    Serial.println("Could not read from EEPROM!");
+  } */
+}
+
 void performLEDPWM()
 {
-  if (!on)
+  if (!onToggle)
   {
     return;
   }
@@ -123,7 +254,7 @@ void performLEDPWM()
   for (int i = 0; i < 1000; i++)
   {
 
-    if (outOneUSeconds >= i)
+    if (on[D5] && outOneUSeconds >= i)
     {
       digitalWrite(D5, HIGH);
     }
@@ -132,7 +263,7 @@ void performLEDPWM()
       digitalWrite(D5, LOW);
     }
 
-    if (outTwoUSeconds >= i)
+    if (on[D6] && outTwoUSeconds >= i)
     {
       digitalWrite(D6, HIGH);
     }
@@ -141,7 +272,7 @@ void performLEDPWM()
       digitalWrite(D6, LOW);
     }
 
-    if (outThreeUSeconds >= i)
+    if (on[D7] && outThreeUSeconds >= i)
     {
       digitalWrite(D7, HIGH);
     }
@@ -155,31 +286,55 @@ void performLEDPWM()
 
 void toggleOnOff()
 {
-  if (on)
+  if (onToggle)
   {
+    Serial.println("Toggle off");
+    onToggle = false;
+    on[D5] = false;
+    on[D6] = false;
+    on[D7] = false;
     digitalWrite(D5, LOW);
     digitalWrite(D6, LOW);
     digitalWrite(D7, LOW);
-    on = false;
   }
   else
   {
-    on = true;
+    Serial.println("Toggle on");
+    on[D5] = true;
+    on[D6] = true;
+    on[D7] = true;
+    onToggle = true;
   }
 }
 
-void increasePWM(int *var, int val)
+void increasePWM(int *var, int val, int pin)
 {
-  if (on && *var < 1000)
+  on[pin] = true;
+  int maxVal = 850;
+
+  if (pin == D5)
+  {
+    maxVal = 1000;
+  }
+
+  if (on[pin] && *var < maxVal)
   {
     *var += val;
+    Serial.printf("increasePWM, pin: %d, val: %d\n", pin, *var);
   }
 }
 
-void decreasePWM(int *var, int val)
+void decreasePWM(int *var, int val, int pin)
 {
-  if (on && *var > 0)
+  if (on[pin] && *var > 0)
   {
+    digitalWrite(pin, HIGH);
     *var -= val;
+    Serial.printf("decreasePWM, pin: %d, val: %d\n", pin, *var);
+  }
+  else
+  {
+    digitalWrite(pin, LOW);
+    on[pin] = false;
   }
 }
